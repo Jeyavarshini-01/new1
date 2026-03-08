819ef126f09ba111018f8ba3f5260a3dd8eca7ec
Jeyavarshini-01 <jeyavarshid57@gmail.com>
2026-03-09 04:24:40 +0530

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Header1 from "./components/Header1"
 import Bodyt from "./components/Bodyt"
 import Projects from "./components/Projects"
-export default function App() {
+export default function APP() {
   return (
     <div>
       <Header1/>
       <Bodyt/>
       <Projects/>
 
-    </div>
+  </div>
   )
+
 }
